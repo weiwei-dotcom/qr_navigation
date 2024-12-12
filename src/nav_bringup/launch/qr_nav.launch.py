9e4744eb82a4f670b59e5fe6dabb9f1cb567345e
weiwei-dotcom <728841808@qq.com>
2024-12-12 21:18:21 +0800

    将点云数据转换成laser_scan数据

--- src/nav_bringup/launch/qr_nav.launch.py
+++ src/nav_bringup/launch/qr_nav.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.action import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
@@ -7,10 +7,10 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    DeclareLaunchArgument(
-        name='scanner', default_value='scanner',
-        description='Namespace for sample topics'
-    )
+    # DeclareLaunchArgument(
+    #     name='scanner', default_value='scanner',
+    #     description='Namespace for sample topics'
+    # )
 
     rviz_config=get_package_share_directory('rslidar_sdk')+'/rviz/rviz2.rviz'
     config_file = '' # your config file path
@@ -21,19 +21,21 @@ def generate_launch_description():
 
     pcl2scan_node = Node(
         package='pointcloud_to_laserscan', node_executable='pointcloud_to_laserscan_node',
-        remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
-                    ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
+        # remappings=[('cloud_in', [LaunchConfiguration(variable_name=''), '/rslidar_points']),
+        #             ('scan', [LaunchConfiguration(variable_name=''), '/scan'])],
+        remappings=[('cloud_in', [ '/rslidar_points']),
+                    ('scan', [ '/scan'])],
         parameters=[{
-            'target_frame': 'cloud',
+            'target_frame': 'base_scan',
             'transform_tolerance': 0.01,
-            'min_height': 0.0,
-            'max_height': 1.0,
-            'angle_min': -1.5708,  # -M_PI/2
-            'angle_max': 1.5708,  # M_PI/2
+            'min_height': -1.0,
+            'max_height': 2.0,
+            'angle_min': 0.0,  # -M_PI/2
+            'angle_max': 6.2831,  # M_PI/2
             'angle_increment': 0.0087,  # M_PI/360.0
             'scan_time': 0.3333,
-            'range_min': 0.45,
-            'range_max': 4.0,
+            'range_min': 0.2,
+            'range_max': 80.0,
             'use_inf': True,
             'inf_epsilon': 1.0
         }],
