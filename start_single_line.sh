f45e741dab6ccf75cfc0c8d2aa3ff6843002f8af
weiwei-dotcom <728841808@qq.com>
2024-12-13 22:51:53 +0800

    更新修复start_single_line.sh

--- start_single_line.sh
+++ start_single_line.sh
@@ -1,3 +1,4 @@
+sudo chmod 777 /dev/ttyUSB0
 gnome-terminal -- bash -c "echo 'start single line lidar!'; source /opt/ros/noetic/setup.bash; cd /home/wl/Documents/ydlidar_ros_driver; source devel/setup.bash; roslaunch ydlidar_ros_driver X2.launch; exec bash"
 sleep 2
 gnome-terminal -- bash -c "echo 'start ros1_bridge!'; source /opt/ros/foxy/setup.bash; source /opt/ros/noetic/setup.bash; ros2 run ros1_bridge dynamic_bridge; exec bash"
