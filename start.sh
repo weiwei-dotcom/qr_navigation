9e22a3176112f1f056ed1e90ad1838b6022c0df8
weiwei-dotcom <728841808@qq.com>
2024-12-15 14:40:09 +0800

    修改launch文件名称使一致,修复在start.sh中调用source.sh文件source的源不能直接在start.sh中生效问题

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ if [ "$1" == 'yd' ]; then
         ros2 launch nav_bringup cartographer.launch.py
     elif [ "$2" == 'amcl' ]; then
         ./yd_pub.sh
-        ros2 launch nav_bringup qr_nav_single.launch.py
+        ros2 launch nav_bringup qr_nav_yd.launch.py
     elif [ "$2" == 'pub' ]; then
         ./yd_pub.sh
         ros2 launch nav_bringup yd_viewer.launch.py
@@ -22,12 +22,12 @@ elif [ "$1" == "rs" ]; then
     if [ "$2" == "cart" ]; then
         ros2 launch nav_bringup cartographer_slam.launch.py
     elif [ "$2" == "amcl" ]; then
-        ros2 launch nav_bringup qr_nav.launch.py
+        ros2 launch nav_bringup qr_nav_rs.launch.py
     elif [ "$2" == 'pub' ]; then
         ros2 launch nav_bringup rs_viewer.launch.py
     fi
 elif [ "$1" == "ros" ]; then
-    ros2 launch nav_bringup ros_default_nav.launch.py
+    ros2 launch nav_bringup ros_nav.launch.py
 else
     echo "please input correct bootargs ! ! !"
 fi
