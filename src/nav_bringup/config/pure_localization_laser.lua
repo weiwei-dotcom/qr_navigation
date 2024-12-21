0d4047f9d3f15c23d5be685c432f535c6b4b7e5c
weiwei-dotcom <728841808@qq.com>
2024-12-21 10:59:38 +0800

    恢复不可编译错误,正在使用cartographer替换amcl的localization

---
+++ src/nav_bringup/config/pure_localization_laser.lua
@@ -0,0 +1,9 @@
+
+include "turtlebot3_lds_2d_localization.lua"
+ 
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+  max_submaps_to_keep = 3,
+}
+POSE_GRAPH.optimize_every_n_nodes = 20
+ 
+return options
