9a2149ecbc1ce30c5d229b886b6d1a4834bfdffb
weiwei-dotcom <728841808@qq.com>
2025-01-02 15:57:15 +0800

    直接从yd_pub.sh中一键启动yd激光雷达并发布查看激光雷达数据

--- yd_pub.sh
+++ yd_pub.sh
@@ -3,3 +3,6 @@ gnome-terminal -- bash -c "echo 'start single line lidar!'; source /opt/ros/noet
 sleep 3
 gnome-terminal -- bash -c "echo 'start ros1_bridge!'; source /opt/ros/foxy/setup.bash; source /opt/ros/noetic/setup.bash; ros2 run ros1_bridge dynamic_bridge; exec bash"
 sleep 2
+source /opt/ros/foxy/setup.bash
+source /install/setup.bash
+ros2 launch laser_pub_pkg yd_viewer.launch.py
